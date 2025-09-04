27dbd1429e54168d6476ad21feca2bcf4f74f529
Yinner Chilito <yinnermedina30@gmail.com>
2025-09-04 09:26:20 -0500

    feat(theme): agregar soporte para cambio de tema y mejorar la estructura de la aplicación
    Se ha implementado un nuevo `ThemeProvider` para gestionar el tema de la aplicación, permitiendo a los usuarios seleccionar entre los modos "light", "dark" y "system". Además, se ha añadido el componente `ModeToggle` en el sidebar para facilitar el cambio de tema. Se han realizado modificaciones en `App.tsx` para incluir el `ThemeProvider` y se ha actualizado el componente `Toaster` para utilizar el nuevo contexto de tema.

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,21 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router';
 import { BrowSerRouter } from './router/browser-router';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { Toaster } from "@/components/ui/sonner"
+import { Toaster } from '@/components/ui/sonner';
+import { ThemeProvider } from './components/theme-provider';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={BrowSerRouter} />
-        <ReactQueryDevtools initialIsOpen={false} />
-        <Toaster richColors closeButton />
-      </QueryClientProvider>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={BrowSerRouter} />
+          <ReactQueryDevtools initialIsOpen={false} />
+          <Toaster richColors closeButton />
+        </QueryClientProvider>
+      </ThemeProvider>
     </>
   );
 }
---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,44 @@
+import { ChevronUp, Moon, Sun } from 'lucide-react';
+
+import { useTheme } from '@/components/theme-provider';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from './ui/sidebar';
+
+export function ModeToggle() {
+  const { setTheme } = useTheme();
+
+  return (
+    <div>
+      <SidebarMenu>
+        <SidebarMenuItem>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <SidebarMenuButton>
+                <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+                <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />{' '}
+                Toggle theme
+                <ChevronUp className="ml-auto" />
+              </SidebarMenuButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem onClick={() => setTheme('light')}>
+                Light
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme('dark')}>
+                Dark
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme('system')}>
+                System
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </SidebarMenuItem>
+      </SidebarMenu>
+    </div>
+  );
+}
