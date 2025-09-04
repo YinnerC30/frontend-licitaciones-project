3942cfd9f0011453a00389df68669375dd202c15
Yinner Chilito <yinnermedina30@gmail.com>
2025-09-04 09:02:01 -0500

    feat(ui): agregar nuevos componentes de interfaz y mejorar la gestión del sidebar
    Se han añadido nuevos componentes como `DropdownMenu`, `Separator`, `Sheet`, `Sidebar`, `Skeleton` y `Tooltip` para mejorar la interfaz de usuario. Además, se ha implementado un contexto para la gestión del estado del sidebar, permitiendo su apertura y cierre. Se han actualizado los archivos `app-layout.tsx` y `home.tsx` para integrar estos nuevos componentes y mejorar la navegación en la aplicación.

--- src/layout/app-layout.tsx
+++ src/layout/app-layout.tsx
@@ -1,7 +1,120 @@
-import { Outlet } from "react-router";
+import {
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar';
 
-const AppLayout = () => {
-  return <Outlet />;
+import { Navigate, Outlet } from 'react-router';
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from '@/components/ui/sidebar';
+import { Calendar, ChevronUp, Home, Inbox, User2 } from 'lucide-react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { useAuthStore } from '@/data/auth-store';
+
+const items = [
+  {
+    title: 'Inicio',
+    url: 'home',
+    icon: Home,
+  },
+  {
+    title: 'Administradores',
+    url: 'administrators',
+    icon: Inbox,
+  },
+  {
+    title: 'Inquilinos',
+    url: 'tenants',
+    icon: Calendar,
+  },
+];
+
+export function AppSidebar() {
+  const { isAuthenticated, logout } = useAuthStore((state) => state);
+
+  if (!isAuthenticated) {
+    return <Navigate to={'../auth'} replace />;
+  }
+
+  return (
+    <Sidebar>
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <User2 /> Username
+                  <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side="top"
+                className="w-[--radix-popper-anchor-width]"
+              >
+                <DropdownMenuItem
+                  onClick={() => {
+                    console.log('Hubo llamado');
+                    logout();
+                  }}
+                >
+                  <span>Cerrar sesión</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
 }
 
-export default AppLayout
+const AppLayout = () => {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        <Outlet />
+      </main>
+    </SidebarProvider>
+  );
+};
+
+export default AppLayout;
--- src/pages/app/home.tsx
+++ src/pages/app/home.tsx
@@ -1,20 +1,10 @@
-import { Button } from '@/components/ui/button';
-import { useAuthStore } from '@/data/auth-store';
-import { Navigate } from 'react-router';
 
 export const Home = () => {
-  const { isAuthenticated, logout } = useAuthStore((state) => state);
-
-  if (!isAuthenticated) {
-    return <Navigate to={'../../auth'} replace />;
-  }
+  
 
   return (
     <div>
       <h1>Home Administration</h1>
-      <Button type="button" onClick={logout}>
-        Logout
-      </Button>
     </div>
   );
 };
