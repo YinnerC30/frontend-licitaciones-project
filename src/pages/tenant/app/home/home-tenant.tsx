11274df31ef65ed413082ad11ce685f98cab35df
Yinner Chilito <yinnermedina30@gmail.com>
2025-09-11 13:32:45 -0500

    feat(home): agregar componente de información de contadores de licitaciones
    Se ha añadido el componente `CountsInformation` en `home-tenant.tsx`, que muestra los contadores de licitaciones aceptadas, cerradas hoy y por criterio. Este cambio mejora la visibilidad de la información relevante para los inquilinos y optimiza la presentación de datos en la interfaz de usuario.

--- src/pages/tenant/app/home/home-tenant.tsx
+++ src/pages/tenant/app/home/home-tenant.tsx
@@ -1,14 +1,58 @@
 import ButtonRefetch from '@/components/button-refetch';
 import { HomeTenantProvider } from '@/context/tenants/home/home-tenant-context';
 import { useGetAllLicitationsByCriteria } from '@/hooks/licitations/use-get-all-licitations-by-criteria';
-import { columnsLicitations } from '../raw-licitations/columns-licitations-table';
-import LicitationsFilterByCriteriaDataTable from './licitations-filter-by-criteria-data-table';
-import CardInfoLicitacion from './card-info-licitation';
 import { useGetLicitationsCounts } from '@/hooks/licitations/use-get-licitations-counts';
+import { columnsLicitations } from '../raw-licitations/columns-licitations-table';
+import CardInfoLicitacion from './card-info-licitation';
+import LicitationsFilterByCriteriaDataTable from './licitations-filter-by-criteria-data-table';
+
+import { CalendarDays, CheckCircle, Filter } from 'lucide-react';
+
+export const CountsInformation = () => {
+  const queryCounts = useGetLicitationsCounts();
+  const { data } = queryCounts;
+
+  return (
+    <>
+      <div className="grid grid-cols-3 gap-4">
+        <div className="flex items-center p-4 rounded-lg bg-green-100 shadow">
+          <CheckCircle className="text-green-600 w-8 h-8 mr-3" />
+          <div>
+            <p className="text-sm text-green-800 font-semibold">
+              Licitaciones aceptadas
+            </p>
+            <p className="text-xl font-bold text-green-900">
+              {data?.valid_licitations ?? 0}
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center p-4 rounded-lg bg-blue-100 shadow">
+          <CalendarDays className="text-blue-600 w-8 h-8 mr-3" />
+          <div>
+            <p className="text-sm text-blue-800 font-semibold">Cerradas hoy</p>
+            <p className="text-xl font-bold text-blue-900">
+              {data?.closed_today_licitations ?? 0}
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center p-4 rounded-lg bg-yellow-100 shadow">
+          <Filter className="text-yellow-600 w-8 h-8 mr-3" />
+          <div>
+            <p className="text-sm text-yellow-800 font-semibold">
+              Por criterio
+            </p>
+            <p className="text-xl font-bold text-yellow-900">
+              {data?.all_licitations_by_criteria ?? 0}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export const HomeTenant = () => {
   const queryByCriteria = useGetAllLicitationsByCriteria();
-  const queryCounts = useGetLicitationsCounts();
 
   if (queryByCriteria.isFetching) {
     return <div>Cargando...</div>;
@@ -18,8 +62,7 @@ export const HomeTenant = () => {
     <div className="grid grid-cols-2 gap-8">
       <HomeTenantProvider>
         <div className="col-span-2">
-          <h1>Contadores de licitaciones</h1>
-          <pre>{JSON.stringify(queryCounts.data, null, 2)}</pre>
+          <CountsInformation />
         </div>
 
         <div className="col-span-2">
