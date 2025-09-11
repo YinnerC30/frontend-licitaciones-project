e3671e8d02eb9278183d3b20902dcc719977d797
Yinner Chilito <yinnermedina30@gmail.com>
2025-09-11 10:29:26 -0500

    feat(licitations): agregar parámetros de consulta al hook de licitaciones seleccionadas
    Se ha modificado el hook `useGetAllLicitationsSelected` para aceptar parámetros de consulta como `limit`, `offset`, `only_accepted` y `all_records`, mejorando la flexibilidad en la obtención de datos. Además, se ha implementado un componente de selección en `licitations-selected.tsx` para permitir al usuario filtrar las licitaciones según estas opciones, optimizando la experiencia de usuario.

--- src/hooks/licitations/use-get-all-licitations-selected.ts
+++ src/hooks/licitations/use-get-all-licitations-selected.ts
@@ -1,14 +1,31 @@
 import { AxiosInstance } from '@/services/axios-service';
 import { useQuery } from '@tanstack/react-query';
 
-const getAllLicitationsSelected = () => {
-  return AxiosInstance.get('/licitations/all-selected');
+interface AllLicitationsSelectedProps {
+  limit?: number;
+  offset?: number;
+  only_accepted?: boolean;
+  all_records?: boolean;
+}
+
+const getAllLicitationsSelected = (data: AllLicitationsSelectedProps) => {
+  const {
+    limit = 10,
+    offset = 0,
+    only_accepted = false,
+    all_records = false,
+  } = data;
+  return AxiosInstance.get('/licitations/all-selected', {
+    params: { limit, offset, only_accepted, all_records },
+  });
 };
 
-export const useGetAllLicitationsSelected = () => {
+export const useGetAllLicitationsSelected = (
+  data: AllLicitationsSelectedProps
+) => {
   const query = useQuery({
-    queryKey: ['licitations-selected'],
-    queryFn: getAllLicitationsSelected,
+    queryKey: ['licitations-selected', data],
+    queryFn: () => getAllLicitationsSelected(data),
     select({ data }) {
       return data;
     },
